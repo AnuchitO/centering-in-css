44cf586adf4a35f888cea01250fe4d72130ca82f
Anuchit Prasertsang <anuchit.prasertsang@gmail.com>
2019-10-20 20:21:33 +0700

    Initial commit

---
+++ src/stack/TicketKata.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { css } from "emotion";
+
+const style = css`
+  width: 400px;
+  height: 150px;
+`;
+
+const TicketKata = () => <div className={style} />;
+
+export default TicketKata;
